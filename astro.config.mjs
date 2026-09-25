8f92a05f93d4c853f2aa814d6a43032e1acf3d47
delauraen <22270079+delauraen@users.noreply.github.com>
2026-09-24 23:27:35 -0700

    adding deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+// https://lalaland.dev
+export default defineConfig({
+    site: 'https://lalaland.dev',
+});
